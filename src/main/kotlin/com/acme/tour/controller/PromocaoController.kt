afcbb753fcce484a27f755e149eb2c366649d173
Lucas Brum <brum.lucas@icloud.com>
2021-02-06 15:38:33 -0300

    First Commit

---
+++ src/main/kotlin/com/acme/tour/controller/PromocaoController.kt
@@ -0,0 +1,31 @@
+package com.acme.tour.controller
+
+import com.acme.tour.model.Promocao
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
+
+@RestController
+class PromocaoController {
+
+    @Autowired
+    lateinit var promocoes: ConcurrentHashMap<Long, Promocao>
+
+    @RequestMapping(value = ["/sayHello"], method = arrayOf(RequestMethod.GET))
+    fun sayHello(): String {
+        return "Hello World"
+    }
+
+    @RequestMapping(value = ["/promocoes/{id}"], method = arrayOf(RequestMethod.GET))
+    fun getById(@PathVariable id: Long) = promocoes[id]
+
+    @RequestMapping(value = ["/promocoes"], method = arrayOf(RequestMethod.POST))
+    fun create(@RequestBody promocao: Promocao) {
+        promocoes[promocao.id] = promocao
+    }
+
+    @RequestMapping(value = ["/promocoes/{id}"], method = arrayOf(RequestMethod.DELETE))
+    fun delete(@PathVariable id: Long) {
+        promocoes.remove(id)
+    }
+}
