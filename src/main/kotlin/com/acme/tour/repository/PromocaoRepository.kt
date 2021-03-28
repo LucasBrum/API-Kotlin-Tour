5cca6d03e3e56a226c9fc9fb59b5f6080974d651
Lucas Brum <brum.lucas@icloud.com>
2021-03-28 11:39:03 -0300

    Atualizando com Querys

--- src/main/kotlin/com/acme/tour/repository/PromocaoRepository.kt
+++ src/main/kotlin/com/acme/tour/repository/PromocaoRepository.kt
@@ -2,10 +2,12 @@ package com.acme.tour.repository
 
 import com.acme.tour.model.Promocao
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
+import javax.transaction.Transactional
 
 interface PromocaoRepository: PagingAndSortingRepository<Promocao, Long> {
 
@@ -14,5 +16,10 @@ interface PromocaoRepository: PagingAndSortingRepository<Promocao, Long> {
 
     @Query(value = "SELECT p.quantidade_dias p WHERE p.local IN :names")
     fun findByLocalInList(@Param("names") names: List<String>): List<Promocao>
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Promocao p SET p.preco = :valor WHERE p.local = :local")
+    fun updateByLocal(@Param("valor") preco: Double, @Param("local") local: String)
 
 }
